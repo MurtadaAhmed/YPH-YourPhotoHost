4dfd86b98941355c6eae2ee62d9615df166f4163
Murtada Ahmed <murtadhabg@gmail.com>
2024-04-03 22:24:26 +0300

    updated allowed hosts

--- murtidjango/settings.py
+++ murtidjango/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-sg5+rz$^-betba=7&*0d_@kco=#%j0cj=tv32p7v7(an0b0lci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
