0d6ccffa68c52939942220b7139c32de4015ec04
Murtada Ahmed <108568451+MurtadaAhmed@users.noreply.github.com>
2024-04-03 23:03:02 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,12 +10,12 @@ sudo apt install python3-pip
 pip install -r requirements.txt
 
 # Run Django management commands
-python manage.py makemigrations
-python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
+python3 manage.py makemigrations
+python3 manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python3 manage.py shell
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Run the Uvicorn server
 sudo uvicorn murtidjango.asgi:application --host 0.0.0.0 --port 80
